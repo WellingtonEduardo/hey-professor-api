0b77b17c6644d5af9fcc4c8b5e93489c5a87fe1c
wellington eduardo <wellingtoneduardo10@outlook>
2024-11-16 12:20:17 -0300

    API-3: validations: name

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Http\Request;
+use Illuminate\Http\{Request, Response};
 
 class RegisterController extends Controller
 {
-    public function __invoke(Request $request): void
+    public function __invoke(Request $request): Response
     {
+        $data = request()->validate([
+            'name' => ['required', 'min:3', 'max:255'],
+
+        ]);
         User::create($request->all());
+
+        return response([], 200);
     }
 }
--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -7,18 +7,40 @@ use function Pest\Laravel\{assertDatabaseHas, postJson};
 use function PHPUnit\Framework\assertTrue;
 
 it('should be able to register in the application', function () {
+
+    $uniqueEmail = uniqid() . 'john.doe@example.com';
+
     postJson(route('register'), [
         'name'     => 'John Doe',
-        'email'    => 'john.doe@example.com',
+        'email'    => $uniqueEmail,
         'password' => 'password',
-    ])->assertSessionHasNoErrors();
+    ])->assertOk();
 
     assertDatabaseHas('users', [
         'name'  => 'John Doe',
-        'email' => 'john.doe@example.com',
+        'email' => $uniqueEmail,
     ]);
 
-    $joeDoe = User::whereEmail('john.doe@example.com')->first();
+    $joeDoe = User::whereEmail($uniqueEmail)->first();
 
     assertTrue(Hash::check('password', $joeDoe->password));
+});
+
+describe('validations', function () {
+
+    test('name', function ($rule, $value) {
+
+        postJson(route('register'), [
+            'name' => $value,
+
+        ])->assertJsonValidationErrors([
+            'name' => $rule,
+        ]);
+
+    })->with([
+        'required' => ['required', ''],
+        'min:3'    => [' at least 3 characters', 'Ab'],
+        'max:255'  => ['greater than 255 characters.', str_repeat('*', 256)],
+    ]);
+
 });
