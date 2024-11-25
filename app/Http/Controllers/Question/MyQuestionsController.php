c0dc245c9790da502dc09d64361d0b393171482e
Wellington Eduardo <138392857+WellingtonEduardo@users.noreply.github.com>
2024-11-25 18:42:47 -0300

    Revert "Feature/api 14"

--- app/Http/Controllers/Question/MyQuestionsController.php
+++ app/Http/Controllers/Question/MyQuestionsController.php
@@ -27,7 +27,7 @@ class MyQuestionsController extends Controller
             fn (Builder $query) => $query->onlyTrashed(),
             fn (Builder $query) => $query->whereStatus($status)
         )
-        ->paginate();
+        ->get();
 
         return QuestionResource::collection($questions);
     }
