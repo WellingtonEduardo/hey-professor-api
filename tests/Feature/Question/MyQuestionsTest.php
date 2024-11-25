c785e77199a04b4ca670d904b7af53e39dc6c59c
Wellington Eduardo <wellingtoneduardo10@outlook.com>
2024-11-25 11:30:35 -0300

    API-25: listing all draft questions related to the logged user

--- tests/Feature/Question/MyQuestionsTest.php
+++ tests/Feature/Question/MyQuestionsTest.php
@@ -31,3 +31,30 @@ it('should list only questions that the logged user has been created :: publishe
         'question' => $otherUserQuestion->question,
     ]);
 });
+
+it('should list only questions that the logged user has been created :: draft', function () {
+    $user              = User::factory()->create();
+    $userQuestion      = Question::factory()->draft()->for($user)->create();
+    $otherUserQuestion = Question::factory()->draft()->create();
+
+    Sanctum::actingAs($user);
+
+    $response = getJson(route('my-questions', ['status' => 'draft']))
+    ->assertOk();
+
+    $response->assertJsonFragment([
+        'id'         => $userQuestion->id,
+        'question'   => $userQuestion->question,
+        'status'     => $userQuestion->status,
+        'created_by' => [
+            'id'   => $userQuestion->user->id,
+            'name' => $userQuestion->user->name,
+        ],
+        'created_at' => $userQuestion->created_at->format('Y-m-d'),
+        'updated_at' => $userQuestion->updated_at->format('Y-m-d'),
+
+        // TODO: add like and unlike count
+    ])->assertJsonMissing([
+        'question' => $otherUserQuestion->question,
+    ]);
+});
