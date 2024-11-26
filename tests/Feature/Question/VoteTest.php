36001d83ee81aa07372bccf6a411b3d901192356
Wellington Eduardo <wellingtoneduardo10@outlook.com>
2024-11-26 09:59:48 -0300

    API-16: should be able to unlike a question

--- tests/Feature/Question/VoteTest.php
+++ tests/Feature/Question/VoteTest.php
@@ -26,3 +26,25 @@ it('should be able to like a question', function () {
     ]);
 
 });
+
+it('should be able to unlike a question', function () {
+    $user = User::factory()->create();
+    Sanctum::actingAs($user);
+
+    $question = Question::factory()->published()->create();
+
+    postJson(route('questions.vote', [
+        'question' => $question,
+        'vote'     => 'unlike',
+    ]))->assertNoContent();
+
+    expect($question->votes)
+    ->toHaveCount(1);
+
+    assertDatabaseHas('votes', [
+        'question_id' => $question->id,
+        'unlike'      => 1,
+        'user_id'     => $user->id,
+    ]);
+
+});
